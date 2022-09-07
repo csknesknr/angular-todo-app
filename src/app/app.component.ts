44c06a4b0d9108911429e54f0106177fea5be2cd
coskuneskiner <csknesknr@gmail.com>
2022-09-08 01:02:19 +0300

    Project İnitial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TodoModel } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo-app';
+  itemList: TodoModel[] = this.getItemsLocalStorage();
+
+
+  addItem(form: NgForm) {
+    this.setItemLocalStorage(form.value);
+    form.reset();
+  }
+
+  setItemLocalStorage(itemText: any) {
+    this.itemList.push(itemText);
+    localStorage.setItem("items", JSON.stringify(this.itemList));
+  }
+
+  getItemsLocalStorage() {
+    if (localStorage.getItem("items") === null) {
+      this.itemList = [];
+    } else {
+      this.itemList = JSON.parse(localStorage.getItem("items") || '[]');
+    }
+    console.log(this.itemList);
+    return this.itemList;
+  }
+
+  deleteItemLocalStorage(todo: TodoModel) {
+    if (confirm("Are you sure ?")) {
+      let index = this.itemList.indexOf(todo);
+      this.itemList.splice(index, 1);
+      localStorage.setItem("items", JSON.stringify(this.itemList));
+    }
+  }
+
+  deleteAllLocalStorage() {
+    if (confirm("Are you sure ?")) {
+      localStorage.clear();
+      this.itemList = [];
+    }
+  }
 }
---
+++ src/app/models/todo.model.ts
@@ -0,0 +1,3 @@
+export class TodoModel {
+    item?: string;
+}
